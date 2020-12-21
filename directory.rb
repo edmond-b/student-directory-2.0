e01d9e3164554fc06c315246937f2e359df5ec51
Edmond <edmond.b@hotmail.co.uk>
2020-12-21 16:39:17 +0000

    DRYed the code

--- directory.rb
+++ directory.rb
@@ -1,22 +1,25 @@
-student_count = 11
+# save the names of the students in an array
+students = [
+  "Dr.Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
-# first we print the names of the students
+# print the names of each student
 puts "The students of Villans Academy"
 puts "-------------"
-puts "Dr.Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each { |student| puts student }
 
 # then we print the number of students
-puts "Overall, we have #{student_count} great students."
+puts "Overall, we have #{students.count} great students."
 
 # manual new lines are done like so
 # print "\nLine 1\nLine 2\nLine 3"
