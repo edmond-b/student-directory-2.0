bb95a874661c88c889106e1c4afa740933928ce4
Edmond <edmond.b@hotmail.co.uk>
2020-12-21 16:23:54 +0000

    added notes

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the names of the students
 puts "The students of Villans Academy"
 puts "-------------"
 puts "Dr.Hannibal Lecter"
@@ -11,3 +12,11 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "MNorman Bates"
+
+# then we print the number of students
+print "Overall, we have "
+print 11
+print " great students."
+
+# manual new lines are done like so
+# print "\nLine 1\nLine 2\nLine 3"
