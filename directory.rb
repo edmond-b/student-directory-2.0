9bb350e5ff64ca8b4894ff0dfc8d0c201f86a5d8
Edmond <edmond.b@hotmail.co.uk>
2020-12-21 16:27:53 +0000

    first refactor

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 # first we print the names of the students
 puts "The students of Villans Academy"
 puts "-------------"
@@ -11,12 +13,10 @@ puts "Terminator"
 puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
-puts "MNorman Bates"
+puts "Norman Bates"
 
 # then we print the number of students
-print "Overall, we have "
-print 11
-print " great students."
+puts "Overall, we have #{student_count} great students."
 
 # manual new lines are done like so
 # print "\nLine 1\nLine 2\nLine 3"
